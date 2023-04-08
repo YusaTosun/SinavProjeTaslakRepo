72f5b3b386a7073ea08279d65582f327144d2a1f
Yusa Tosun <yusatosun.yt@gmail.com>
2023-04-08 18:09:33 +0300

    first push

---
+++ SinavProjeTaslak/Data/Context/BlogContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SinavProjeTaslak.Data.Configurations;
+using SinavProjeTaslak.Data.Entities;
+
+namespace SinavProjeTaslak.Data.Context
+{
+    public class BlogContext:IdentityDbContext<AppUser,AppRole,int>
+    {
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+        public BlogContext(DbContextOptions<BlogContext> options):base(options) 
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new BlogConfigurations());
+        }
+
+    }
+}
