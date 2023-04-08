90a72d57a59a148c855daf27b583bc9e75613923
Yusa Tosun <yusatosun.yt@gmail.com>
2023-04-08 17:13:53 +0300

    Add project files.

---
+++ SinavProjeTaslak/Data/Entities/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace SinavProjeTaslak.Data.Entities
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public ICollection<Blog> Blogs { get; set; }
+
+        public Author()
+        {
+            Blogs = new List<Blog>();
+        }
+    }
+}
---
+++ SinavProjeTaslak/Data/Entities/Blog.cs
@@ -0,0 +1,11 @@
+﻿namespace SinavProjeTaslak.Data.Entities
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogHeader { get; set; }
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
